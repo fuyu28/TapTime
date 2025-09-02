a2b26e3319edf4e3c537317627c0277a6becd46f
fuyu <88916226+fuyu28@users.noreply.github.com>
2025-09-02 20:06:39 +0900

    feat:Drizzleセットアップ

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "sqlite",
+  schema: "./src/schema.ts",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "file:./data/app.db",
+  },
+  verbose: true,
+  strict: true,
+});
